969eb9fe81330cfc69d612e63f95ec66536da5da
Sarthak Jadhav <sarthak140706@gmail.com>
2025-11-23 15:56:23 +0530

    Delete assignment1_task1.py

--- assignment1_task1.py
+++
@@ -1,6 +0,0 @@
-a=int(input("enter the first number: "))
-b=int(input("enter the second number: "))
-print("Addition: ",a+b)
-print("Subtraction: ",a-b)
-print("Multiplication: ",a*b)
-print("Division: ",a/b)
